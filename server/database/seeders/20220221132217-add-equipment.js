d9f803c572e4df464d0b8beea49c093d13d91321
Jake <jacobminor@hotmail.co.uk>
2022-02-21 14:19:04 +0000

    FYP-16 Add Equipment seed script

---
+++ server/database/seeders/20220221132217-add-equipment.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Equipment', [
+      {
+        name: "Treadmill"
+      },
+      {
+        name: "Dumbbells"
+      },
+      {
+        name: "Barbell"
+      },
+      {
+        name: "Bar"
+      },
+      {
+        name: "Resistance bands"
+      },
+      {
+        name: "Resistance cables"
+      },
+      {
+        name: "Battle ropes"
+      },
+      {
+        name: "Static bike"
+      },
+      {
+        name: "Yoga mat"
+      },
+      {
+        name: "Medicine ball"
+      },
+      {
+        name: "Indoor rower"
+      },
+      {
+        name: "Kettle bell"
+      },
+      {
+        name: "Skipping rope"
+      }
+    ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Equipment', null, {});
+  }
+};
