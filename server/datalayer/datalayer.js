99b05276c21760ffb31ce427fbbcc686534fea6a
Jake <jacobminor@hotmail.co.uk>
2022-02-23 20:18:35 +0000

    FYP-16 Create Datalayer and find one method

---
+++ server/datalayer/datalayer.js
@@ -0,0 +1,10 @@
+﻿module.exports = class DataLayer {
+ constructor(modelName) {
+  this.model = require(`/server/database/models/${modelName}`)
+ }
+ 
+ async findOne(filter) {
+  return this.model.findOne(filter)
+    .catch(error => throw error)
+ }
+}
