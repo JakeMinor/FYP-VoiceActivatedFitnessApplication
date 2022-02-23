dd9397a3c8bbeaacb41283c118aa7063c113d95d
Jake <jacobminor@hotmail.co.uk>
2022-02-23 20:30:08 +0000

    FYP-16 Create business logic to get workout by name

---
+++ server/business/workout.business.js
@@ -0,0 +1,9 @@
+﻿import DataLayer from '../datalayer/datalayer'
+import Exercise from '../database/models/exercise'
+const DataLayer = new DataLayer('workout')
+
+module.exports = class WorkoutBusiness {
+ async getWorkoutByName(name) {
+  return DataLayer.findOne({ where: { name: name }, include: Exercise()})
+ }
+}
