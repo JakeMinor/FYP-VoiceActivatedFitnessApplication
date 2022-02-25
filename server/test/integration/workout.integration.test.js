30442be19989e1a0cb351702051a6dd9bac950ad
Jake <jacobminor@hotmail.co.uk>
2022-02-25 15:15:53 +0000

    FYP-16 Add console.log to help fix pipeline

--- server/test/integration/workout.integration.test.js
+++ server/test/integration/workout.integration.test.js
@@ -18,6 +18,7 @@ describe('Workout Integration Tests', () => {
    
    //Act
    const result = await chai.request(server).get(`${baseUrl}${workoutName}`)
+   console.log(result)
    
    //Assert
    result.should.have.status(200)
