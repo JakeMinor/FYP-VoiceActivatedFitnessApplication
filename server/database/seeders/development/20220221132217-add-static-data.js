f423c12ad909172ac72b72fed026d4d8d3a86c3a
Jake <jacobminor@hotmail.co.uk>
2022-02-27 16:43:09 +0000

    Change directory so it can be used on the pipeline

--- server/database/seeders/development/20220221132217-add-static-data.js
+++ server/database/seeders/development/20220221132217-add-static-data.js
@@ -1,5 +1,5 @@
 'use strict';
-const {equipment, exercises, workouts} = require('../config/data')
+const {equipment, exercises, workouts} = require('/server/database/config/data')
 
 module.exports = {
   async up (queryInterface, Sequelize) {
