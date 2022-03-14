af709ad70b2f4882221f9e539a31d9addeff5402
Jake <jacobminor@hotmail.co.uk>
2022-03-14 15:29:14 +0000

    FYP-8 Add filtering to completed workouts table

--- ui/src/views/CompletedWorkouts.vue
+++ ui/src/views/CompletedWorkouts.vue
@@ -1,7 +1,29 @@
 ﻿<template>
   <div>
     <Title title="Completed Workouts" type="Page" />
-    <b-table :fields="tableHeaders" :items="this.$data.workouts">
+    <b-table :fields="tableHeaders" :items="filteredItems" show-empty empty-text="No workouts could be found.">
+      <template #head(name)="head">
+        {{head.label}}
+        <b-input id="nameFilter" v-model="filters.name" size="sm" class="mt-2" placeholder="Name..."></b-input>
+      </template>
+      <template #head(numberOfExercisesCompleted)="head">
+        {{ head.label }}
+        <b-input id="numberOfExercisesCompletedFilter" type="number" min="0" oninput="validity.valid||(value='');" v-model="filters.exercisesCompleted" size="sm" class="mt-2" placeholder="Exercises completed..."></b-input>
+      </template>
+      <template #head(completedDate)="head">
+        {{ head.label }}
+        <date-picker id="dateCompletedFilter" v-model="filters.completedDate" mode="date" :model-config="config">
+          <template v-slot="{ inputValue, inputEvents }">
+            <b-input :value="inputValue" v-on="inputEvents" size="sm" class="mt-2" placeholder="Completed Date..."/>
+          </template>
+        </date-picker>
+      </template>
+      <template #head(actions)="head">
+        <div class="d-flex flex-column">
+          {{ head.label }}
+          <b-button class="mt-3 text-left p-0 font-weight-normal" variant="link" @click="resetFilters">Reset filters</b-button>
+        </div>
+      </template>
       <template #cell(completedDate)="cell">
         {{ formatDate(cell.item.completedDate) }}
       </template>
@@ -17,10 +39,11 @@ import Vue from 'vue';
 import Title from "../components/Title.vue";
 import api from '../api/api'
 import { formatDate } from '@/helper';
+import { DatePicker } from 'v-calendar'
 
 export default Vue.extend({
   name: "CompletedWorkouts.vue",
-  components: { Title },
+  components: { Title, DatePicker },
   data() {
     return {
       workouts: [], // The users workouts.
@@ -28,11 +51,21 @@ export default Vue.extend({
         name: '',
         exercisesCompleted: '',
         completedDate: ''
+      },
+      config: {
+        type: 'string',
+        mask: 'iso'
       }
     }
   },
   methods: {
-    formatDate
+    formatDate,
+    resetFilters() {
+      
+      this.filters.name = ''
+      this.filters.exercisesCompleted = ''
+      this.filters.completedDate = ''
+    }
   },
   computed: {
     tableHeaders() {
@@ -43,6 +76,12 @@ export default Vue.extend({
         { key: "actions", sortable: false }
       ]
     },
+    filteredItems() {
+      return this.$data.workouts.filter((workout) =>
+          workout.name.includes(this.filters.name) &&
+          workout.completedDate.split('T')[0].includes(this.filters.completedDate.split('T')[0]) &&
+          (this.filters.exercisesCompleted !== '' ? this.filters.exercisesCompleted === workout.numberOfExercisesCompleted : true ))
+    }
   },
   /**
    * Gets the users workout statistics from the api.
