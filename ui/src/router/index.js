a137942903ad84916c577cdbf9a598e1965dd246
Jake <jacobminor@hotmail.co.uk>
2022-03-18 10:01:42 +0000

    Remove unauthenticated redirect

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -45,29 +45,29 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach(async (to, from, next) => {
-  // Check if the access token cookie exists.
-  if (document.cookie !== ''){
-    // Get the token from the cookie.
-    const token = document.cookie.split('=')[1]
-    
-    // Get the authenticated users details from the amazon api.
-    await api.getUser(token)
-    
-    // If the user has been set, continue to the route.
-    if(store.getters['user'] !== null) {
-      next()
-    } 
-    // Else reauthenticate as the token has expired.  
-    else { 
-      await api.authenticate()
-    }
-  } 
-  // Else authenticate with the system.
-  else {
-    await api.authenticate()
-  }
-})
+// router.beforeEach(async (to, from, next) => {
+//   // Check if the access token cookie exists.
+//   if (document.cookie !== ''){
+//     // Get the token from the cookie.
+//     const token = document.cookie.split('=')[1]
+//
+//     // Get the authenticated users details from the amazon api.
+//     await api.getUser(token)
+//
+//     // If the user has been set, continue to the route.
+//     if(store.getters['user'] !== null) {
+//       next()
+//     } 
+//     // Else reauthenticate as the token has expired.  
+//     else { 
+//       await api.authenticate()
+//     }
+//   } 
+//   // Else authenticate with the system.
+//   else {
+//     await api.authenticate()
+//   }
+// })
 
 
 export default router
