5f3c6e643ca2aaa7495449f4527b01aab0813d30
Jake <jacobminor@hotmail.co.uk>
2022-03-21 14:30:13 +0000

    FYP-33 Add Swagger Documentation

--- server/router/router.config.js
+++ server/router/router.config.js
@@ -3,6 +3,10 @@ const statisticRoutes = require("./routes/statistic")
 const authRoutes = require("./routes/auth")
 const noteRoutes = require("./routes/note")
 
+// Swagger UI
+const swaggerUi = require('swagger-ui-express'),
+  swaggerDocument = require('../swagger.json')
+
 /**
  * Define the different routes for the API.
  */
@@ -11,4 +15,5 @@ module.exports = (app) => {
  app.use('/v1/statistic', statisticRoutes)
  app.use('/v1/auth', authRoutes)
  app.use('/v1/note', noteRoutes)
+ app.use('/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 }
